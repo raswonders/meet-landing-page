7a7cfa3b453e1c92257430a77ab711ec2db19992
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-22 18:34:18 +0200

    expand VerticalIndicator to be able receive styles

--- src/components/VerticalIndicator.tsx
+++ src/components/VerticalIndicator.tsx
@@ -1,12 +1,15 @@
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-export function VerticalIndicator({ children }: Props) {
+export function VerticalIndicator({ children, className }: Props) {
   return (
-    <div className="flex flex-col items-center font-black text-slate-600">
+    <div
+      className={`flex flex-col items-center font-black text-slate-600 ${className}`}
+    >
       <div className="min-h-20 border border-l-slate-300"></div>
-      <div className="w-14 h-14 border border-slate-300 rounded-full flex justify-center items-center">
+      <div className="w-14 h-14 border bg-white border-slate-300 rounded-full flex justify-center items-center">
         {children}
       </div>
     </div>
