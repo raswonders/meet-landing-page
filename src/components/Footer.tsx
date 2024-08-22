d072568c74d37c2a606907bac58afce73d588db4
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-22 18:34:34 +0200

    add mobile styles for Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,23 @@
 import { Button } from "./Button";
+import { VerticalIndicator } from "./VerticalIndicator";
 
 export function Footer() {
   return (
-    <footer className="">
-      <div>
-        <h3>Experience more together</h3>
-        <p className="">
-          Stay connected with reliable HD meetings and unlimited one-on-one and
-          group video sessions.
-        </p>
-        <Button color="purple">Download 1.3</Button>
-      </div>
-    </footer>
+    <>
+      <VerticalIndicator className="-mb-7">02</VerticalIndicator>
+      <footer className="relative -z-20 py-16 px-8 bg-cover bg-[url('/assets/mobile/image-footer.jpg')]">
+        <div className="absolute -z-10 inset-0 bg-cyan-600 opacity-90"></div>
+        <div className="flex flex-col justify-center text-white gap-6 text-center">
+          <h3 className="text-4xl font-black">Experience more together</h3>
+          <p className="font-medium">
+            Stay connected with reliable HD meetings and unlimited one-on-one
+            and group video sessions.
+          </p>
+          <Button color="purple">
+            Download <span className="text-purple-300">v1.3</span>
+          </Button>
+        </div>
+      </footer>
+    </>
   );
 }
