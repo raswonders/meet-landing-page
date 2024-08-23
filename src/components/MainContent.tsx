b7ff1b66419555a3fcfce8d7c63de73f66bba07a
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-23 15:12:05 +0200

    add desktop styles to MainContent

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,9 +2,9 @@ import { VerticalIndicator } from "./VerticalIndicator";
 
 export function MainContent() {
   return (
-    <main className="p-8 sm:pt-14 pb-16 flex flex-col items-center">
+    <main className="p-8 sm:pt-14 pb-16 lg:pb-[4.5rem] lg:px-20 flex flex-col items-center">
       <VerticalIndicator>01</VerticalIndicator>
-      <div className="my-16 grid grid-cols-2 sm:grid-cols-4 grid-rows-2 sm:grid-rows-1 gap-4">
+      <div className="my-16 lg:mx-20 grid grid-cols-2 sm:grid-cols-4 grid-rows-2 sm:grid-rows-1 gap-4 lg:gap-5">
         <img
           src="/assets/mobile/image-woman-in-videocall.jpg"
           alt=""
@@ -27,14 +27,14 @@ export function MainContent() {
         />
       </div>
 
-      <div className="flex flex-col items-center text-center max-w-lg">
-        <h2 className="text-cyan-600 text-xs font-black uppercase tracking-[0.25rem] mb-4 leading-[1.1]">
+      <div className="flex flex-col items-center text-center max-w-lg lg:max-w-xl">
+        <h2 className="text-cyan-600 text-xs sm:text-sm lg:text-base font-black uppercase tracking-[0.25rem] mb-4 leading-[1.1]">
           Built for modern use
         </h2>
-        <h3 className="mb-8 text-4xl font-black">
+        <h3 className="mb-8 text-[2rem] sm:text-4xl lg:text[2.5rem] font-black leading-tight">
           Smarter meetings all in one place
         </h3>
-        <p className="font-medium text-slate-600">
+        <p className="font-medium lg:text-lg text-slate-600">
           Send messages, share files, show your screen, and record your meetings
           — all in one workspace. Control who can join with invite-only team
           access, data encryption, and data export.
