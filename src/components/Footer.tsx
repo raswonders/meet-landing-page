bdebe019ae60cefb6ef35a2d224d68d3ccac8848
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-23 13:27:07 +0200

    add tablet styles to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ export function Footer() {
   return (
     <>
       <VerticalIndicator className="-mb-7">02</VerticalIndicator>
-      <footer className="relative -z-20 py-16 px-8 bg-cover bg-[url('/assets/mobile/image-footer.jpg')]">
+      <footer className="relative -z-20 py-16 px-8 flex flex-col items-center bg-cover bg-[url('/assets/mobile/image-footer.jpg')]">
         <div className="absolute -z-10 inset-0 bg-cyan-600 opacity-90"></div>
-        <div className="flex flex-col justify-center text-white gap-6 text-center">
+        <div className="flex flex-col items-center text-white gap-6 text-center max-w-lg">
           <h3 className="text-4xl font-black">Experience more together</h3>
           <p className="font-medium">
             Stay connected with reliable HD meetings and unlimited one-on-one
