d88eede222bc523a74e98c412e78287255b2a406
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-23 14:08:45 +0200

    add desktop styles to Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,10 @@ import { Button } from "./Button";
 
 export function Hero() {
   return (
-    <section className="pt-16 pb-8 sm:pb-16 space-y-12 overflow-hidden">
-      <div className="-mx-[7.5vw] aspect-[820/303] bg-cover bg-[url('/assets/mobile/image-hero.png')]"></div>
-      <div className="flex justify-center">
+    <section className="pt-16 pb-8 sm:pb-16 space-y-12 lg:space-y-0 overflow-hidden lg:flex lg:justify-between lg:items-center">
+      <div className="lg:hidden -mx-[7.5vw] aspect-[820/303] bg-cover bg-[url('/assets/mobile/image-hero.png')]"></div>
+      <div className="hidden w-1/4 self-start -ml-[3vw] lg:block aspect-[394/303] bg-cover bg-[url('/assets/desktop/image-hero-left.png')]"></div>
+      <div className="flex justify-center lg:shrink-0">
         <div className="px-9 flex flex-col gap-6 items-center text-center max-w-md">
           <h1 className="text-slate-900 font-black text-4xl">
             Group Chat for Everyone
@@ -21,6 +22,7 @@ export function Hero() {
           </div>
         </div>
       </div>
+      <div className="hidden w-1/4 self-end -mr-[3vw] lg:block aspect-[394/303] bg-cover bg-[url('/assets/desktop/image-hero-right.png')]"></div>
     </section>
   );
 }
