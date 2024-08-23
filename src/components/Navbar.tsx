192ae578eb3f917e2dd6569dff94e332c0683fa3
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-23 13:35:04 +0200

    add desktop styles to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export function Navbar() {
   return (
-    <header className="flex justify-center">
-      <img src="/assets/logo.svg" alt="Logo" width="118" className="mt-12" />
+    <header className="flex justify-center pt-12 lg:pt-20">
+      <img src="/assets/logo.svg" alt="Logo" width="118"/>
     </header>
   );
 }
