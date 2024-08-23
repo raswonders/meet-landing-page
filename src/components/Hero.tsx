ac67530b83347a59d27e21173f9451170d7989c4
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-23 15:18:09 +0200

    fix: fonts on hero section aren't responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,11 @@ export function Hero() {
       <div className="lg:hidden -mx-[7.5vw] aspect-[820/303] bg-cover bg-[url('/assets/mobile/image-hero.png')]"></div>
       <div className="hidden w-1/4 self-start -ml-[3vw] lg:block aspect-[394/303] bg-cover bg-[url('/assets/desktop/image-hero-left.png')]"></div>
       <div className="flex justify-center lg:shrink-0">
-        <div className="px-9 flex flex-col gap-6 items-center text-center max-w-md">
-          <h1 className="text-slate-900 font-black text-4xl">
+        <div className="px-9 flex flex-col gap-6 items-center text-center max-w-md lg:max-w-lg">
+          <h1 className="text-slate-900 font-black text-[2.5rem] sm:text-5xl lg:text-[4rem] leading-tight">
             Group Chat for Everyone
           </h1>
-          <p className="font-medium text-slate-600">
+          <p className="font-medium lg:text-lg text-slate-600">
             Meet makes it easy to connect with others face-to-face virtually and
             collaborate across any device.
           </p>
