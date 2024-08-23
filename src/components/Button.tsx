a551a52d21043d421780ddc010a0fbd1745a030b
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-23 16:47:46 +0200

    correct footer pos and add button interaction

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,17 +7,17 @@ export function Button({ color, children }: Props) {
   let bgStyle = "";
   switch (color) {
     case "cyan":
-      bgStyle = "bg-cyan-600";
+      bgStyle = "bg-cyan-600 hover:bg-[#71C0D4]";
       break;
     case "purple":
     default:
-      bgStyle = "bg-purple-600";
+      bgStyle = "bg-purple-600 hover:bg-[#B18BDD]";
       break;
   }
 
   return (
     <button
-      className={`${bgStyle} py-4 px-10 rounded-[2rem] font-black text-white whitespace-nowrap`}
+      className={`${bgStyle} transition-colors duration-700 ease-in-out py-4 px-10 rounded-[2rem] font-black text-white whitespace-nowrap`}
     >
       {children}
     </button>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ import { VerticalIndicator } from "./VerticalIndicator";
 export function Footer() {
   return (
     <>
-      <VerticalIndicator className="-mb-7">02</VerticalIndicator>
-      <footer className="relative -z-20 py-16 px-4 lg:py-28 lg:px-40 flex flex-col items-center bg-cover bg-[url('/assets/mobile/image-footer.jpg')]">
-        <div className="absolute -z-10 inset-0 bg-cyan-600 opacity-90"></div>
-        <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-between text-white gap-6 text-center lg:text-start max-w-lg lg:max-w-full">
+      <VerticalIndicator className=" relative z-10 -mb-7">02</VerticalIndicator>
+      <footer className="relative py-16 px-4 lg:py-28 lg:px-40 flex flex-col items-center bg-cover bg-[url('/assets/mobile/image-footer.jpg')]">
+        <div className="absolute inset-0 bg-cyan-600 opacity-90"></div>
+        <div className="relative flex flex-col lg:flex-row items-center lg:items-start lg:justify-between text-white gap-6 text-center lg:text-start max-w-lg lg:max-w-full">
           <h3 className="lg:basis-1/3 text-[2rem] sm:text-4xl lg:text[2.5rem] font-black">
             Experience more together
           </h3>
